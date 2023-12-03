20494e2d0647cfd0bfc3b50efa2dfef323120acb
Jason Schweier <jason.schweier@gmail.com>
2023-12-03 14:37:23 -0500

    Add solve task to print results

---
+++ lib/mix/tasks/solve.ex
@@ -0,0 +1,20 @@
+defmodule Mix.Tasks.Solve do
+  def run([day, part]) do
+    day = String.to_integer(day)
+    part = String.to_integer(part)
+
+    result = solve(day, part)
+
+    IO.puts("Day #{day}, part #{part}: #{result}")
+  end
+
+  defp solve(2, 1) do
+    import AdventOfCode2023.Day02CubeConundrum
+
+    Input.strings(2)
+    |> Enum.map(&parse_game/1)
+    |> Enum.filter(&possible?(&1, [red: 12, green: 13, blue: 14]))
+    |> Enum.map(fn {game_id, _sets} -> game_id end)
+    |> Enum.sum()
+  end
+end
